cf969f4f690ec2d0b488f3712398292d0670f02f
rungmc <gregmcm@gmail.com>
2021-08-26 22:46:31 -0400

    Rudiementary game done. Not the most elegant but uses all the BS I could think to cram in without making an ordeal of it.

--- rps.js
+++ rps.js
@@ -25,9 +25,35 @@ function playRound(playerSelection, computerSelection){
     else if(playerSelection.toLowerCase() === 'scissors'){
         return (computerSelection === 'Paper') ? 'You Win! Scissors beats Paper' : 'You Lose! Rock beats Scissors';
     }
+    else {
+        return 'Invalid play!';
+    }
 
 }
 
-function game(){
-    const rounds = 5;
+function game(rounds = 5){
+    let wins = 0;
+    let losses = 0;
+
+    for (let i = 0; i < rounds; i++){
+        let playerSelection = prompt('What are you throwing?');
+        let computerSelection = computerPlay();
+        if (playRound(playerSelection, computerSelection).slice(4,7) === 'Win'){
+            wins++;
+        }
+        else if (playRound(playerSelection, computerSelection).slice(4,8) === 'Lose'){
+            losses++;
+        }
+        console.log(playRound(playerSelection, computerSelection));
+    }
+
+    if (wins > losses){
+        console.log('You won with a score of ' + wins + ' - ' + losses);
+    }
+    else if (losses > wins){
+        console.log('You lost with a score of ' + wins + ' - ' + losses);
+    }
+    else {
+        console.log('You tied with a score of ' + wins + ' - ' + losses);
+    }
 }
